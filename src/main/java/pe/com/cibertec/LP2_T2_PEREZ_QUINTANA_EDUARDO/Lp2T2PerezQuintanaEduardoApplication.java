98feb18a53c5ae23b4b42e1255ce90629f397050
edupq1999 <nivel.17@hotmail.com>
2024-10-12 20:27:32 -0500

    Se creo el proyecto

---
+++ src/main/java/pe/com/cibertec/LP2_T2_PEREZ_QUINTANA_EDUARDO/Lp2T2PerezQuintanaEduardoApplication.java
@@ -0,0 +1,13 @@
+package pe.com.cibertec.LP2_T2_PEREZ_QUINTANA_EDUARDO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2PerezQuintanaEduardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2PerezQuintanaEduardoApplication.class, args);
+	}
+
+}
